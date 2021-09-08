7f566ffb8a030f3896bdf2869e93a6bcd1d7d59c
Andrea Angola <3469@holbertonschool.com>
2021-09-08 15:41:40 -0700

    list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
         print("{} agument:".format(arg - 1))
     else:
         print("{} arguments:".format(arg - 1))
-    for i in range(1, len(sys.argv)):
+    for i in range(1, arg):
         print("{}: {}".format(i, sys.argv[i]))
