374e5eefe2b27351c7f91a81dacd13a3881f2f53
Andrea Angola <3469@holbertonschool.com>
2021-09-14 19:22:52 -0700

    pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         for columna in fila:
             print("{}".format(columna), end="")
             if columna != fila[len(fila) - 1]:
-                print("", end="")
+                print(" ", end="")
         print("")
