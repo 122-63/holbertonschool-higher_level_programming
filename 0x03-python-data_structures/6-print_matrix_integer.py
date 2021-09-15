1a63977cf61dc4247f64430f4c9488458c8b25cf
Andrea Angola <3469@holbertonschool.com>
2021-09-14 19:19:23 -0700

    prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for fila in matrix:
+        for columna in fila:
+            print("{}".format(columna), end="")
+            if columna != fila[len(fila) - 1]:
+                print("", end="")
+        print("")
