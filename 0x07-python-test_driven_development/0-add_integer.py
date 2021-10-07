7df3f476c76fcc66da5944a371de3db7765b3121
Andrea Angola <3469@holbertonschool.com>
2021-10-06 22:18:19 -0700

    corretions

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,17 @@
 
 def add_integer(a, b=98):
     """the function returns the sum of the integers"""
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
+
+    res = a + b
+    if res == float('inf') or res == -float('inf'):
+        return 89
     return (int(a) + int(b))
