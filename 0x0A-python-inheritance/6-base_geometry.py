e25c78205f3aabebe2f9a67d53b7afef9173ea9e
Andrea Angola <3469@holbertonschool.com>
2021-10-11 19:50:03 -0700

    error message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""..."""
+
+
+class BaseGeometry:
+    """..."""
+    def area(self):
+        """message area() is not implemented"""
+        raise Exception("area() is not implemented")
