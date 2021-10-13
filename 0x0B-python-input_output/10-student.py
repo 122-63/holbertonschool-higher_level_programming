ee76eb18eea3ab175c6fee8764e10680024fb38c
Andrea Angola <3469@holbertonschool.com>
2021-10-12 21:31:44 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+task 10
+"""
+
+
+class Student:
+    """empty class"""
+
+    def __init__(self, first_name, last_name, age):
+        """new Student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """a dictionary representation of a Student"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
