8bdfdcb8d81b703ea6ae28d687836636b6ee904d
Andrea Angola <3469@holbertonschool.com>
2021-10-18 15:05:08 -0700

    returns the dictionary representation of a Rectangle and Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,3 +49,12 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """return the dictionary representation of the Square"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+         }
