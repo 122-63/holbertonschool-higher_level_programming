d8a2a833fa9b892f84ff2c7fcab9de1251a8b57b
Andrea Angola <3469@holbertonschool.com>
2021-10-18 12:14:31 -0700

    assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,22 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """update the rectangle"""
+
+        if len(args) != 0:
+            i = 0
+            for i, arg in enumerate(args):
+                if i == 0:
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+                i += 1
