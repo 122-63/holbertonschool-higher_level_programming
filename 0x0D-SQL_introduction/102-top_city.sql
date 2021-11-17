4e0aedca1753945f94e9c2a2366c6f9ba515f2f5
Andrea Angola <3469@holbertonschool.com>
2021-11-16 20:55:00 -0800

    Import in hbtn_0c_0 database

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT city, AVG(value) AS avg_tem
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
