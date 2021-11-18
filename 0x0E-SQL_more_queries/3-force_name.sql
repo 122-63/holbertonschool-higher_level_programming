491ffc62e034a9c7ac1c8658ce2c4809935cfbf1
Andrea Angola <3469@holbertonschool.com>
2021-11-17 19:15:47 -0800

    creates the table force_name on your MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- creates the table force_name on your MySQL server
+-- id INT
+-- name VARCHAR(256) cant be null
+CREATE TABLE IF NOT EXISTS force_name
+(
+    id int NOT NULL,
+    name varchar(256) NOT NULL
+);
