157ec2495076b6c6c5780428211abc30e3a660c7
Andrea Angola <3469@holbertonschool.com>
2021-11-17 20:25:51 -0800

    lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesnt have a genre, display NULL
+SELECT tv_shows.title, genre_id	FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
