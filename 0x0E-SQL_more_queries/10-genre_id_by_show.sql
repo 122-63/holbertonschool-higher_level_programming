0111412ecc6de89876fc464a0877e4143c5492ee
Andrea Angola <3469@holbertonschool.com>
2021-11-17 20:17:05 -0800

    update lists all shows contained in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,8 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT state.title, genre.genre_id FROM tv_shows
-INNER JOIN tv_show_genres ON state.id = genre.show_id
-ORDER BY state.title, genre.genre_id;
+SELECT tv_shows.title, genre_id FROM tv_show_genres
+INNER JOIN tv_genres ON tv_genres.id = genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER by tv_shows.title, tv_show_genres.genre_id;
+
