13bd0da15130b324e656f62920363ead8b652f24
Andrea Angola <3469@holbertonschool.com>
2021-12-13 19:53:07 -0800

    Write a script that prints 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines
+// using an array of string and a loop
+for (const langs of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(langs);
+}
