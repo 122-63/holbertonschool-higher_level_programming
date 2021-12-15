e64e13ea4c0d94628745d2e430587c40d0ee68f4
Andrea Angola <3469@holbertonschool.com>
2021-12-14 20:06:05 -0800

    eturns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let i;
+  const newList = [];
+  for (i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
