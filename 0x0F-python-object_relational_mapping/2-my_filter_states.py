59d8657c68551f28357ea0667e41df2fc34ec4a2
Andrea Angola <3469@holbertonschool.com>
2021-12-19 17:33:26 -0800

    correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ and state name searched
 import MySQLdb
 from sys import argv
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
