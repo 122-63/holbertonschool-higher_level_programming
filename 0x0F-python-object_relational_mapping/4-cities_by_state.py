b838258bb6d6e09d7fa9d2f11313d71e326e0221
122-63 <3469@holbertonschool.com>
2021-12-24 13:00:52 -0500

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELET cities.id, cities.name, states.name FROM cities"
-                "INNER JOIN states ON states.id = cities.state_id"
-                "ORDER BY cities.id ASC")
-    for cities in cur.fetchall():
-        print(cities)
+                "INNER JOIN states ON cities.state_id = states.id;")
+    for city in cur.fetchall():
+        print(city)
     cur.close()
     db.close()
