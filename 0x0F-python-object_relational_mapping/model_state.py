026b0778f23d646e63ea191af85cbcc98fa0f708
122-63 <3469@holbertonschool.com>
2021-12-24 13:26:48 -0500

    update task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,10 +3,12 @@
 contains the class definition of a State and an
 instance Base = declarative_base()
 """
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     """State table"""
