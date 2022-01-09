10b4c818c09c4c5ff8ef515ce173756533806bc9
122-63 <3469@holbertonschool.com>
2022-01-09 14:29:47 -0500

    takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sv -X OPTIONS "$1"
+curl -si -X OPTIONS -L "$1" | grep "access-control-allow-methods" | sed "s/access-control-allow-methods\: //g"
