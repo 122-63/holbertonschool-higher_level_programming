97885b1d6f2dc0b5ea92f0b28968262e498f3e52
122-63 <3469@holbertonschool.com>
2022-01-09 20:42:17 -0500

    sends a request to the URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# sends a request to the URL and displays the value of the X-Request-Id
+"""sends a request to the URL and displays the value of the X-Request-Id"""
 import urllib.request
 from sys import argv
 
