631ed5ec3c2d9e81f955699ee391895e1046fde1
122-63 <3469@holbertonschool.com>
2022-01-09 23:27:36 -0500

    POST - email - requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+POST - email - requests 
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    data = requests.post(url, email)
+    print(data.text)
