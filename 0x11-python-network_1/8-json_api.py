c1d1b0c1434a8aef23819f32833ae05be3fd78a5
122-63 <3469@holbertonschool.com>
2022-01-11 20:38:34 -0500

    sends a POST request with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,20 +3,22 @@
 sends a POST request with the letter as a parameter
 """
 
-from sys import argv
-import requests
-
-
 if __name__ == "__main__":
-    if len(argv) == 2:
-        arg = argv[1]
+    import requests
+    from sys import argv
 
+    URL = "http://0.0.0.0:5000/search_user"
+
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    dic = {'q': q}
     try:
-        url = requests.post("b991a1b5a53e.c3bcb5f8.hbtn-cod.io:5000/search_user", data={'q': arg})
-        response = url.json()
-        if response == {}:
+        req = requests.post(URL, dic).json()
+        if len(req) == 0 or not req.get('id') or not req.get('name'):
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(req.get('id'), req.get('name')))
     except ValueError:
         print("Not a valid JSON")
