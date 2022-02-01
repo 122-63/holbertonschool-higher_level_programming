0420e8a8d2efaf8996330d753d93664ffbfa6295
122-63 <3469@holbertonschool.com>
2022-02-01 18:44:38 -0500

    display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const https = require('https');
+
+const req = https.request(process.argv[2], res => {
+  console.log(`code: ${res.statusCode}`);
+});
+
+req.on('error', error => {
+  console.log(`code: ${error.statusCode}`);
+});
+
+req.end();
