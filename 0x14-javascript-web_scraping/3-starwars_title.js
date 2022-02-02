fc09f2ad470d409862017cc088421a452d0344de
122-63 <3469@holbertonschool.com>
2022-02-01 21:03:06 -0500

    The first argument is the movie ID

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,10 +2,11 @@
 const request = require('request');
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url).on('response', response => {
-  response.setEncoding('UTF8');
-  response.on('data', data => {
-    const movie = JSON.parse(data);
-    console.log(movie.title);
-  });
+request.get(url, (err, res, body) => {
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+
+  if (err) {
+    console.error(err);
+  }
 });
